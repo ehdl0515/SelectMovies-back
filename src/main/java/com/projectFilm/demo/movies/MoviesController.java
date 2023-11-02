4620259ac4225e111002fc85db6b14199023e834
곽동희 <ehdl0515@hamonsoft.co.kr>
2023-11-03 08:44:32 +0900

    [feat.mod (movies)] API 수정

--- src/main/java/com/projectFilm/demo/movies/MoviesController.java
+++ src/main/java/com/projectFilm/demo/movies/MoviesController.java
@@ -2,8 +2,6 @@ package com.projectFilm.demo.movies;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,21 +30,11 @@ public class MoviesController {
 	public Object PostOneList(@RequestBody int movieCd) {
 		try {
 			Optional<Movies> result = moviesRepository.findById(String.valueOf(movieCd));
-			System.out.println(result);
-
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("Content-Type", "application/json");
-
-			// ResponseEntity로 응답 생성
-			ResponseEntity<Optional<Movies>> rtn = ResponseEntity.ok()
-					.headers(headers)
-					.body(result);
-			System.out.println(rtn);
-			return rtn;
+			log.info(String.valueOf(result));
+			return result;
 
 		} catch (Exception e) {
-//			log.error(e.toString());
-			System.out.println(e.toString());
+			log.error(e.toString());
 			return Optional.empty();
 
 		}
