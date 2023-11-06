6bcdcbe711a3caa11432af11ea6ba3b18091b0c2
KDH <ehdl0515@naver.com>
2023-11-06 14:03:08 +0900

    [feat.mod (movies)] API 수정
    - repository 사용자 메서드 추가

--- src/main/java/com/projectFilm/demo/movies/MoviesController.java
+++ src/main/java/com/projectFilm/demo/movies/MoviesController.java
@@ -2,6 +2,9 @@ package com.projectFilm.demo.movies;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +19,28 @@ public class MoviesController {
 	private MoviesRepository moviesRepository;
 
 	@GetMapping("/movies")
-	public List<Movies> ResponseAllList() {
+	public Page<Movies> ResponseAllListWithPage(@RequestParam(required = false, defaultValue = "0") int page) {
 		try {
-			return moviesRepository.findAll();
+			return moviesRepository.findAll(PageRequest.of(page, 10, Sort.by("openDt").descending()));
 		} catch (Exception e) {
 			log.error(e.toString());
 			return null;
-
 		}
-
 	}
+
+	@GetMapping("/movies/all")
+	public long ResponseAllList() {
+		try {
+//			List<Movies> result = moviesRepository.findMoviesWithLimit(175);
+			long result = moviesRepository.count();
+//			log.info(String.valueOf(result));
+			return result;
+		} catch (Exception e) {
+			log.error(e.toString());
+			return 0;
+		}
+	}
+
 	@PostMapping("/movies/one")
 	public Object PostOneList(@RequestBody int movieCd) {
 		try {
--- src/main/java/com/projectFilm/demo/movies/MoviesRepository.java
+++ src/main/java/com/projectFilm/demo/movies/MoviesRepository.java
@@ -1,9 +1,14 @@
 package com.projectFilm.demo.movies;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface MoviesRepository extends JpaRepository<Movies, String> {
+	@Query(value = "SELECT * FROM Movies LIMIT :limit", nativeQuery = true)
+	List<Movies> findMoviesWithLimit(int limit);
 }
